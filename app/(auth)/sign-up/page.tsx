8af40982acdf490607ab4262d83762ace5845af6
naji-97 <elbairabennaji@gmail.com>
2025-11-18 17:16:25 +0100

    allow the user to use all character

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -64,7 +64,7 @@ const SignUp = () => {
           placeholder="contact@jsmastery.com"
           register={register}
           error={errors.email}
-          validation={{ required: 'Email name is required', pattern: /^\w+@\w+\.\w+$/, message: 'Email address is required' }}
+          validation={{ required: 'Email name is required', pattern: /^[\w+]+@\w+\.\w+$/, message: 'Email address is required' }}
         />
 
         <InputField
